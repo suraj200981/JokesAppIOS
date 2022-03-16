2ff40836ed4967469abc0a4a36147f3f8ffa63c8
suraj <suraj200981@yahoo.co.uk>
2022-03-16 13:38:52 +0000

    category buttons created

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, Image, TouchableOpacity} from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
         <Text style={styles.homeTitle}>Pick a category</Text>
+        <View style={styles.containerForCats1}>
+        <TouchableOpacity style={styles.categoryCard}>
+        <Text>Ionicons Icons</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.categoryCard}>
+        <Text>Ionicons Iconsdss</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.containerForCats2}>
+        <TouchableOpacity style={styles.categoryCard}>
+        <Text>Ionicons Icons</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.categoryCard}>
+        <Text>Ionicons Iconsdss</Text>
+        </TouchableOpacity>
+        </View>
     </View>
 
-      );
+  );
 };
 
 const styles = StyleSheet.create({
@@ -15,12 +31,28 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: 'white',
     marginTop:40,
-    fontWeight: '100'
+    fontWeight: '100',
+    textAlign: 'center'
+
   },
   container: {
     flex: 1,
-    alignItems: 'center',
     backgroundColor: '#C70E4F',
+  },
+  categoryCard:{
+    width:150,
+    height:300,
+    backgroundColor:'rgba(76, 5, 30, 0.72)',
+    borderRadius:10,
+    marginTop:20,
+    marginBottom:20,
+    marginLeft:31,
+  },
+  containerForCats1:{
+    flexDirection:'row',
+  },
+  containerForCats2:{
+    flexDirection:'row',
   }
 });
 
