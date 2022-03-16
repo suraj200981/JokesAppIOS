67970a86190cb087980fa074d6fbd06ddb4e19c5
suraj <suraj200981@yahoo.co.uk>
2022-03-16 14:12:45 +0000

    progress on random joke screen

--- src/screens/RandomJokeScreen.js
+++ src/screens/RandomJokeScreen.js
@@ -4,9 +4,17 @@ import { Text, StyleSheet, View, Image, TouchableOpacity} from 'react-native';
 const RandomJokeScreen = () => {
   return (
     <View style={styles.container}>
-     <View>
+     <View style={styles.jokesWindow}>
 
      </View>
+        <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Back</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Next</Text>
+            </TouchableOpacity>
+        </View>
     </View>
 
   );
@@ -17,6 +25,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#C70E4F',
   },
+  jokesWindow: {
+    backgroundColor: 'white',
+    width: 300,
+    height: 450,
+    borderRadius: 10,
+    marginTop: 30,
+    //align in the center of the screen
+    alignSelf: 'center',
+    //border color
+    borderColor: 'black',
+    //border width
+    borderWidth: 2,
+    },
 
 });
 
